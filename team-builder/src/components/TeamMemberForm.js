f07f6bf4e5db1d90e53bb6fe00f125482380ae33
Joe <josephc.lambert@gmail.com>
2019-09-18 22:04:53 -0700

    adds ability to create a TeamMember in the TeamMemberForm. Sets the TeamMember to the form state.

--- team-builder/src/components/TeamMemberForm.js
+++ team-builder/src/components/TeamMemberForm.js
@@ -1,11 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 
 const TeamMemberForm = props => {
+  const [teamMember, setTeamMember] = useState({
+    name: "",
+    email: "",
+    role: ""
+  });
+
+  const handleChange = e => {
+    console.log(e.target.value);
+    setTeamMember({
+      ...teamMember,
+      [e.target.name]: e.target.value
+    });
+  };
+
   return (
     <form>
       <div>
         <label htmlFor="name">Name</label>
-        <input type="text" id="name" name="name" placeholder="name" />
+        <input
+          type="text"
+          id="name"
+          name="name"
+          placeholder="name"
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="email">E-mail</label>
+        <input
+          type="text"
+          id="email"
+          name="email"
+          placeholder="name@example.com"
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="role">Role</label>
+        <input
+          type="text"
+          id="role"
+          name="role"
+          placeholder="Role"
+          onChange={handleChange}
+        />
       </div>
     </form>
   );
